34ae8fd839d9b94b8b4d40e6c26458311915e403
valli0x <youarethesame1821@gmail.com>
2023-12-24 19:07:01 +0300

    license file

--- cmd/main.go
+++ cmd/main.go
@@ -22,6 +22,8 @@ func main() {
 		Mailer: mailer,
 	})
 
-	ctx, _ := context.WithTimeout(context.Background(), 5 * time.Minute)
+	ctx, ctxcancel := context.WithTimeout(context.Background(), 5 * time.Minute) // :)
+	defer ctxcancel()
+
 	go server.Run(ctx)
 }
